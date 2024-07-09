b0a845dcdcb97447e89b7df70b6a175791336cd8
NRI酒井 将大 <m2-sakai@nri.co.jp>
2024-07-09 15:38:23 +0900

    add loading UI

--- app/root.tsx
+++ app/root.tsx
@@ -8,6 +8,7 @@ import {
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  useNavigation,
 } from '@remix-run/react';
 
 import appStylesHref from './app.css?url';
@@ -30,6 +31,7 @@ export const loader = async () => {
 
 export default function App() {
   const { contacts } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
 
   return (
     <html lang="en">
@@ -87,7 +89,10 @@ export default function App() {
             )}
           </nav>
         </div>
-        <div id="detail">
+        <div
+          className={navigation.state === 'loading' ? 'loading' : ''}
+          id="detail"
+        >
           <Outlet />
         </div>
 
