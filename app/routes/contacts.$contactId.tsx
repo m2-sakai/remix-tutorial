94b6f89ac8b84665d56ea5738854c2bb78180ded
NRI酒井 将大 <m2-sakai@nri.co.jp>
2024-07-09 13:59:55 +0900

    update param

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,17 +1,17 @@
-import { Form } from '@remix-run/react';
+import { Form, json, useLoaderData } from '@remix-run/react';
 import type { FunctionComponent } from 'react';
 
 import type { ContactRecord } from '../data';
 
+import { getContact } from '../data';
+
+export const loader = async ({ params }) => {
+  const contact = await getContact(params.contactId);
+  return json({ contact });
+};
+
 export default function Contact() {
-  const contact = {
-    first: 'Your',
-    last: 'Name',
-    avatar: 'https://placekitten.com/200/200',
-    twitter: 'your_handle',
-    notes: 'Some notes',
-    favorite: true,
-  };
+  const { contact } = useLoaderData<typeof loader>();
 
   return (
     <div id="contact">
