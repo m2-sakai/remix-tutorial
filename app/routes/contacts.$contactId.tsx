6f7723ca7102b9b724d51d1b379f02a2a18f2d31
NRI酒井 将大 <m2-sakai@nri.co.jp>
2024-07-09 14:03:53 +0900

    check ts error

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -4,9 +4,15 @@ import type { FunctionComponent } from 'react';
 import type { ContactRecord } from '../data';
 
 import { getContact } from '../data';
+import { LoaderFunctionArgs } from '@remix-run/node';
+import invariant from 'tiny-invariant';
 
-export const loader = async ({ params }) => {
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  invariant(params.contactId, 'Missing contactId param');
   const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response('Not Found', { status: 404 });
+  }
   return json({ contact });
 };
 
