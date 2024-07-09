1e7e5a8d8ed6d253df69c85d351e07b7e71e5f2e
NRI酒井 将大 <m2-sakai@nri.co.jp>
2024-07-09 15:27:17 +0900

    add data

--- app/root.tsx
+++ app/root.tsx
@@ -12,7 +12,12 @@ import {
 
 import appStylesHref from './app.css?url';
 
-import { getContacts } from './data';
+import { createEmptyContact, getContacts } from './data';
+
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return json({ contact });
+};
 
 export const links: LinksFunction = () => [
   { rel: 'styleSheet', href: appStylesHref },
