840e86aaafb3786bb8b1bd3894ffc12fc50f61bc
NRI酒井 将大 <m2-sakai@nri.co.jp>
2024-07-16 08:35:39 +0900

    add edit cancel

--- app/routes/contacts.$contactId_.edit.tsx
+++ app/routes/contacts.$contactId_.edit.tsx
@@ -1,6 +1,6 @@
 import type { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node';
 import { json, redirect } from '@remix-run/node';
-import { Form, useLoaderData } from '@remix-run/react';
+import { Form, useLoaderData, useNavigate } from '@remix-run/react';
 import invariant from 'tiny-invariant';
 
 import { getContact, updateContact } from '../data';
@@ -24,6 +24,7 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
 
 export default function EditContact() {
   const { contact } = useLoaderData<typeof loader>();
+  const navigate = useNavigate();
 
   return (
     <Form key={contact.id} id="contact-form" method="post">
@@ -69,7 +70,9 @@ export default function EditContact() {
       </label>
       <p>
         <button type="submit">保存</button>
-        <button type="button">キャンセル</button>
+        <button onClick={() => navigate(-1)} type="button">
+          キャンセル
+        </button>
       </p>
     </Form>
   );
