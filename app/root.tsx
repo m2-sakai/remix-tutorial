66a3761ec59208496ba7f1693112536397799d57
NRI酒井 将大 <m2-sakai@nri.co.jp>
2024-07-16 08:52:17 +0900

    add onChange in dorm

--- app/root.tsx
+++ app/root.tsx
@@ -14,6 +14,7 @@ import {
   ScrollRestoration,
   useLoaderData,
   useNavigation,
+  useSubmit,
 } from '@remix-run/react';
 
 import appStylesHref from './app.css?url';
@@ -40,6 +41,7 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 export default function App() {
   const { contacts, q } = useLoaderData<typeof loader>();
   const navigation = useNavigation();
+  const submit = useSubmit();
 
   useEffect(() => {
     const searchField = document.getElementById('q');
@@ -60,7 +62,11 @@ export default function App() {
         <div id="sidebar">
           <h1>Remix Contacts</h1>
           <div>
-            <Form id="search-form" role="search">
+            <Form
+              id="search-form"
+              onChange={(event) => submit(event.currentTarget)}
+              role="search"
+            >
               <input
                 aria-label="Search contacts"
                 defaultValue={q || ''}
