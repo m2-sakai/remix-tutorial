ba6779a193674b3f5377d30173d8a86900914e68
NRI酒井 将大 <m2-sakai@nri.co.jp>
2024-07-16 08:38:37 +0900

    search

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,9 @@
-import { json, LinksFunction, redirect } from '@remix-run/node';
+import {
+  json,
+  LinksFunction,
+  LoaderFunctionArgs,
+  redirect,
+} from '@remix-run/node';
 import {
   Form,
   Links,
@@ -24,8 +29,10 @@ export const links: LinksFunction = () => [
   { rel: 'styleSheet', href: appStylesHref },
 ];
 
-export const loader = async () => {
-  const contacts = await getContacts();
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get('q');
+  const contacts = await getContacts(q);
   return json({ contacts });
 };
 
