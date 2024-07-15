ddf75c2840d20c704ad1b1b8b0ccc5e8eedf9ef9
NRI酒井 将大 <m2-sakai@nri.co.jp>
2024-07-16 08:33:12 +0900

    add index route

---
+++ app/routes/_index.tsx
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <p id="index-page">
+      これはRemixのデモです。
+      <br />
+      <a href="https://remix.run">remix.runのドキュメント</a>をご覧ください。
+    </p>
+  );
+}
